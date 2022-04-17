92c2dcd0dd0fd6a29ed7ddb8d8fc45d39e692ddc
ny-ki <71499617+ny-ki@users.noreply.github.com>
2022-04-17 20:15:19 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int d[1001][3];
+int r[1001];
+int g[1001];
+int b[1001];
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n,s_n;
+  cin>>n;
+
+  for(int i=1;i<=n;i++){
+    cin>>r[i]>>g[i]>>b[i];
+  }
+  
+  d[1][0]=r[1];
+  d[1][1]=g[1];
+  d[1][2]=b[1];
+
+  for(int i=2;i<n+1;i++){
+    d[i][0]=min(d[i-1][1],d[i-1][2])+r[i];
+    d[i][1]=min(d[i-1][0],d[i-1][2])+g[i];
+    d[i][2]=min(d[i-1][0],d[i-1][1])+b[i];
+  }
+  cout<<min({d[n][0],d[n][1],d[n][2]})<<'\n';
+  return 0;
+}
